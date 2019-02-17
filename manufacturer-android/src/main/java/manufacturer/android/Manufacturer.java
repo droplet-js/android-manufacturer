f81f96d51c0a0fda09427b5926f0e5dca12ee73b
v7lin <v7lin@qq.com>
2019-02-17 21:27:47 +0800

    android-manufacturer

---
+++ manufacturer-android/src/main/java/manufacturer/android/Manufacturer.java
@@ -0,0 +1,41 @@
+package manufacturer.android;
+
+import android.os.Build;
+import android.text.TextUtils;
+
+import java.util.Locale;
+
+public enum Manufacturer {
+    UNKNOWN("未知", ""),
+    HUAWEI("华为", "huawei"),
+    MEIZU("魅族", "meizu"),
+    XIAOMI("小米", "xiaomi"),
+    OPPO("OPPO", "oppo"),
+    VIVO("VIVO", "vivo"),
+    SAMSUNG("三星", "samsung"),
+    SONY("索尼", "sony"),
+    LG("LG", "lg"),
+    LETV("乐视", "letv"),
+    ZTE("中兴", "zte"),
+    YULONG("酷派", "yulong"),
+    LENOVO("联想", "lenovo");
+
+    private final String alias;
+    private final String manufacturer;
+
+    Manufacturer(String alias, String manufacturer) {
+        this.alias = alias;
+        this.manufacturer = manufacturer;
+    }
+
+    public static Manufacturer recognizer() {
+        Manufacturer recognizer = UNKNOWN;
+        for (Manufacturer manufacturer : Manufacturer.values()) {
+            if (manufacturer != UNKNOWN && TextUtils.equals(manufacturer.manufacturer, Build.MANUFACTURER.toLowerCase(Locale.getDefault()))) {
+                recognizer = manufacturer;
+                break;
+            }
+        }
+        return recognizer;
+    }
+}
